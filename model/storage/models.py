f0277823328ab931a3fbbf5c4b45cf8028a0adc8
benelungo <dovgaliuk.pavlo@gmail.com>
2025-08-15 01:12:02 +0300

    Models final

--- model/storage/models.py
+++ model/storage/models.py
@@ -1,5 +1,9 @@
+import datetime
+from typing import Optional
+
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column, String, Date, JSON, Integer, UniqueConstraint
+from sqlalchemy import Column, String, Date, JSON, Integer, UniqueConstraint, DateTime, Float, Boolean, SmallInteger
 
 Base = declarative_base()
 
@@ -9,47 +13,27 @@ class Models:
         __tablename__ = "queries"
         query = Column(String, primary_key=True)
 
-
     class ProductFeatures(Base):
         __tablename__ = "product_features"
+
         id = Column(Integer, primary_key=True, autoincrement=True)
-        query = Column(String, nullable=False, index=True)
-        product_id = Column(String, nullable=False, index=True)
-        data = Column(JSON, nullable=False)
-        data_hash = Column(String(64), nullable=False, index=True)
-        __table_args__ = (
-            UniqueConstraint("query", "product_id", name="uq_product_features_qpid"),
-        )
+        data = Column(JSONB, nullable=False)
+
 
     class ProductTexts(Base):
         __tablename__ = "product_texts"
+
         id = Column(Integer, primary_key=True, autoincrement=True)
-        query = Column(String, nullable=False, index=True)
-        product_id = Column(String, nullable=False, index=True)
-        data = Column(JSON, nullable=False)
-        data_hash = Column(String(64), nullable=False, index=True)
-        __table_args__ = (
-            UniqueConstraint("query", "product_id", name="uq_product_texts_qpid"),
-        )
+        data = Column(JSONB, nullable=False)
 
     class ReviewFeatures(Base):
         __tablename__ = "review_features"
+
         id = Column(Integer, primary_key=True, autoincrement=True)
-        query = Column(String, nullable=False, index=True)
-        product_id = Column(String, nullable=False, index=True)
-        data = Column(JSON, nullable=False)
-        data_hash = Column(String(64), nullable=False, index=True)
-        __table_args__ = (
-            UniqueConstraint("query", "product_id", name="uq_review_features_qpid"),
-        )
+        data = Column(JSONB, nullable=False)
 
     class ReviewTexts(Base):
         __tablename__ = "review_texts"
+
         id = Column(Integer, primary_key=True, autoincrement=True)
-        query = Column(String, nullable=False, index=True)
-        product_id = Column(String, nullable=False, index=True)
-        data = Column(JSON, nullable=False)
-        data_hash = Column(String(64), nullable=False, index=True)
-        __table_args__ = (
-            UniqueConstraint("query", "product_id", name="uq_review_texts_qpid"),
-        )
+        data = Column(JSONB, nullable=False)

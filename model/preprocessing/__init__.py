15a80163749e057ca5224edd8cdf52ee8023c88e
benelungo <dovgaliuk.pavlo@gmail.com>
2025-08-19 10:02:28 +0300

    Preprocessing __init__ updated

--- model/preprocessing/__init__.py
+++ model/preprocessing/__init__.py
@@ -1,4 +1,6 @@
 from . import products
 from . import reviews
+from . import save_to_db
+from . import load_from_db
 
-__all__ = ["products", "reviews"]
+__all__ = ["products", "reviews", "save_to_db", "load_from_db"]
